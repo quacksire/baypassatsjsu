233721e836dcd4a488778d7c020260e4d3399232
burritosoftware <37681398+burritosoftware@users.noreply.github.com>
2025-08-19 23:52:23 -0700

    Capitalize logo text

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     return (
         <div style={{fontFamily: "'Archivo Black'", textAlign: "center", transform: "translateY(2px)"}} className={`drop-shadow-[1.5px_1.5px_0_var(--color-auggold)]`}>
             <div style={{fontSize: "10pt", marginBottom: "-12px"}}>
-                Baypass at
+                BayPass at
             </div>
             <div style={{fontSize: "21pt"}}>
                 SJSU
